3e5ae8ef6d91ede361cb9709ea3e1a84a7d5aa72
jhonatan.arias <jhonatan.arias@onoff.com.co>
2024-04-27 22:52:23 -0500

    Comment legend

---
+++ graphics/bar_graphic.r
@@ -0,0 +1,46 @@
+# PACKAGES
+install.packages("tidyr")
+isntall.packages("dplyr")
+istall.packages("ggplot2")
+install.packages("readxl")
+install.packages("RColorBrewer")
+
+# LIBRARIES
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(readxl)
+library(RColorBrewer)
+
+# EXAMPLE 1
+Tipo_de_operacion <- c(
+  "Toracica",
+  "Huesos y articulaciones",
+  "Ojo, oido, nariz y garganta",
+  "General",
+  "Abdominal",
+  "Urologica",
+  "Practologica",
+  "Neurocirugia")
+Numero_de_casos <- c(20, 45, 58, 98, 115, 74, 65, 23)
+Id_operacion <- c(1, 2, 3, 4, 5, 6, 7, 8)
+operationTable <- data.frame(
+  Tipo_de_operacion,
+  Numero_de_casos, 
+  Id_operacion
+)
+View(operationTable)
+
+legendText <- paste(operationTable$Id_operacion, operationTable$Tipo_de_operacion, sep=": ")
+
+barplot(
+  operationTable$Numero_de_casos,
+  names.arg = operationTable$Id_operacion,
+  legend.text = legendText,
+  axisnames = TRUE,
+  col = "blue",
+  main = "Numero de casos por tipo de operación",
+  xlab = "Tipo de operación",
+  ylab = "Numero de casos"
+)
+
